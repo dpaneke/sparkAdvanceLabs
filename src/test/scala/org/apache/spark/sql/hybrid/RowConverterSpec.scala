41db38d8d0e922c09719ed772f1162d75050e277
David Paneque <davidpaneque@MacBook-Pro-David.local>
2025-02-07 00:05:41 +0300

    Added convert Df Test

--- src/test/scala/org/apache/spark/sql/hybrid/RowConverterSpec.scala
+++ src/test/scala/org/apache/spark/sql/hybrid/RowConverterSpec.scala
@@ -1,26 +1,47 @@
 package org.apache.spark.sql.hybrid
 
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.{Dataset, SparkSession}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+import org.apache.log4j.Logger
 
 import java.lang
 
 class RowConverterSpec extends AnyFlatSpec with should.Matchers {
 
+  val log = Logger.getLogger(getClass)
+
   val spark: SparkSession =
     SparkSession
       .builder()
-      .master("local[1]")
+      .master("local[*]")
       .appName("test")
       .getOrCreate()
 
   "Converter" should "work" in {
-    val data: Dataset[lang.Long] = spark.range(1)
+    val data: Dataset[lang.Long] = spark.range(3)
     val rows: Iterator[InternalRow] = data.queryExecution.toRdd.collect().toIterator
     val converter: RowConverter = new RowConverter(data.schema)
     val out: Iterator[String] = converter.toJsonString(rows)
+    out.toList.foreach(println)
+  }
+
+  "Converter" should "convert Df" in {
+    val df = spark.createDataFrame(spark.sparkContext.parallelize(Seq(
+      Row(1, "Alice", "x"),
+      Row(10, "Bob", "y"),
+      Row(100, "John", "y")
+    )),
+      schema=StructType(
+        Seq(StructField("id", IntegerType), StructField("name", StringType), StructField("xy", StringType))
+      ))
+
+    val rows: Iterator[InternalRow] = df.queryExecution.toRdd.toLocalIterator
+    val converter: RowConverter = new RowConverter(df.schema)
+    val out: Iterator[String] = converter.toJsonString(rows)
+    println(out.toList)
     out.toList.foreach(println)
   }
 }
